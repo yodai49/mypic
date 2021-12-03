ca8ddf5275748016c3584141a3b85012f3d01d79
yodai49 <cbasicjavaasembler@yahoo.co.jp>
2021-12-04 00:34:42 +0900

    template

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-var t = 0; //time
-var mode = 0; //mode 0タイトル画面　1セレクト画面　2プレイ画面　3リザルト画面　
-var animationmax = 1; //nextmodeにうつるタイミング
-var animationcnt=0; //アニメーションのカウンター 1でスタート -1処理前
-var nextmode=0; //次にうつるモード
-
-// ページの読み込みを待つ
-
+var mode = 0; //mode 0タイトル画面　1探索画面　2戦闘画面
+var nextMode=0; //次に遷移するモード
+var modeAnimation = 1; //nextmodeにうつるタイミング
+var messageWindow=0;//メッセージウィンドウの表示非表示
+var myPicWindow=0;//マイピク描画画面の表示非表示
+var menuWindow=0;//メニューウィンドウの表示非表示
+var shopWindow=0;//ショップウィンドウの表示非表示
+const width = 960;
+const height = 540;
 
 function keypress(mykey,mykeycode){ //キー入力イベント
     if(mykey=="z"){
@@ -21,12 +22,6 @@ window.addEventListener('DOMContentLoaded', function(){ ///キー入力イベン
 });
 
 function init() {
-    //ローディング処理////////////////////////////////////////
-
-    // サイズを指定
-    const width = 960;
-    const height = 540;
-
     //2Dの処理
     ctx2d=document.getElementById("myCanvas").getContext("2d");
     ctx2d.width = width;
@@ -34,14 +29,13 @@ function init() {
 
     tick();
 
-    function tick() {
-        t++;//システム系の処理
+    function tick() { //メイン関数
         //2次元のリセット処理
         ctx2d.clearRect(0,0,width,height);
 
         ctx2d.fillStyle="rgba(255,255,0,1.0)";
-        ctx2d.fillRect(30+t,30,30,30);
+        ctx2d.fillRect(30,30,30,30);
 
-        requestAnimationFrame(tick);
+        requestAnimationFrame(tick); //次のフレーム呼び出し（再帰）
     }
 }

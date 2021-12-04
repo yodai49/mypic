13ca30992aac3584bc587adf4190f64ab779f43f
yodai49 <cbasicjavaasembler@yahoo.co.jp>
2021-12-04 17:48:08 +0900

    walk conlision

--- field.js
+++ field.js
@@ -1,9 +1,9 @@
 var walkanimation=0; //歩くアニメーション
-const charasize=30; //キャラクターのサイズ
+const charasize=60; //キャラクターのサイズ
 const pre_charasize=60; //プリレンダリング用のキャラクターのサイズ
 const fieldwidth=4000;//フィールドの幅の最大値
 const fieldheight=4000;//フィールドの高さの最大値
-const walkspeed=3;//歩くスピード
+var walkspeed=3;//歩くスピード
 
 function initiate_field(){
     /*　フィールド・キャラクターの初期化処理
@@ -34,29 +34,56 @@ function initiate_field(){
         }
     }
 
-    myposx=200,myposy=200,myposworld=0;//ポジションのセッティング
+    myposx=500,myposy=500,myposworld=0;//ポジションのセッティング
 }
 function myposmodify(){
     /*　自分の位置がはみ出さないようにする関数
         param なし
         return なし
     */
-    if(myposx<10) myposx=10;
+    if(myposx-(width-charasize)/2<10) myposx=10+(width-charasize)/2;
     if (myposx>fieldwidth-10) myposx=fieldwidth-10;
-    if(myposy<10) myposy=10;
+    if(myposy-(height-charasize)/2<10) myposy=10+(height-charasize)/2;
     if (myposy>fieldheight-10) myposx=fieldheight-10;
+}
+function walkeve(){ //歩くときに発生する処理
+    myposmodify();
+    walkanimation=(walkanimation+1)%30; //歩く処理
+}
+function checkConflict(dir){
+    /* 当たり判定
+    @param dir--移動しようとする方向　0-3で、左右上下の順番
+    @return 0-衝突なし　1-衝突あり
+    */
+    var checkConflictPosx=0,checkConflictPosy=0;
+    if (dir==0) checkConflictPosx= -walkspeed-1,checkConflictPosy=0;
+    if (dir==1) checkConflictPosx= charasize+walkspeed+1,checkConflictPosy=0;
+    if (dir==2) checkConflictPosx= 0,checkConflictPosy=-walkspeed-1;
+    if (dir==3) checkConflictPosx= 0,checkConflictPosy=charasize+walkspeed+1;
+    var tempColision = 0;
+    for(let i = 0;i < walkCol[myposworld].length;i++){
+        tempColision = 0;
+        for(let j = 0;j < 10;j++){
+            var checkimgdata=ctx2d.getImageData((width-charasize)/2+checkConflictPosx,(height-charasize)/2+checkConflictPosy,1,1);
+            if (dir==2 || dir == 3) checkConflictPosx+=(charasize/10);
+            if (dir==0 || dir == 1) checkConflictPosy+=(charasize/10);
+            if (checkimgdata.data[0] != walkCol[myposworld][i][0] || checkimgdata.data[1] != walkCol[myposworld][i][1] ||  checkimgdata.data[2] != walkCol[myposworld][i][2]) tempColision=1;
+        }
+        if (!tempColision) return 0;
+    }
+    return 1;
 }
 function fieldMain() {
     /*
     @param なし
     @return なし
     */
-    if (leftkey) myposx-=walkspeed;
-    if (rightkey) myposx+=walkspeed;
-    if (upkey) myposy-=walkspeed;
-    if (downkey) myposy+=walkspeed;
-    myposmodify();
-    walkanimation=(walkanimation+1)%30; //歩く処理
-    ctx2d.drawImage(fieldcanvas,myposx,myposy,width,height,0,0,width,height); //背景の描画
-    ctx2d.drawImage(characanvas,60*Math.floor(walkanimation/15),0,60,60,(width-charasize)/2,(height-charasize)/2,60,60); //キャラクターの描画
+    ctx2d.drawImage(fieldcanvas,myposx-(width-charasize)/2,myposy-(height-charasize)/2,width,height,0,0,width,height); //背景の描画
+    ctx2d.drawImage(characanvas,charasize*Math.floor(walkanimation/15),0,charasize,charasize,(width-charasize)/2,(height-charasize)/2,charasize,charasize); //キャラクターの描画
+
+    //移動処理
+    if (leftkey && !checkConflict(0)) myposx-=walkspeed,walkeve();
+    if (rightkey && !checkConflict(1)) myposx+=walkspeed,walkeve();
+    if (upkey && !checkConflict(2)) myposy-=walkspeed,walkeve();
+    if (downkey && !checkConflict(3)) myposy+=walkspeed,walkeve();
 }

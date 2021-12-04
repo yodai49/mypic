649e107498b849cbc9f7bb767917594c24597ffd
yodai49 <cbasicjavaasembler@yahoo.co.jp>
2021-12-04 14:51:12 +0900

    walkman_animation

--- field.js
+++ field.js
@@ -1,3 +1,5 @@
+var walkanimation=0;
+
 function initiate_field(){
     /*　フィールド・キャラクターの初期化処理
     @param なし
@@ -7,10 +9,10 @@ function initiate_field(){
     charaimg1.src="./imgs/character_field1.png";
     charaimg2.src="./imgs/character_field2.png";
     characanvas=document.createElement("canvas");
-    characanvas.width=64, characanvas.height=64
-    characanvas=characanvas.getContext("2d"); //charaimg1は0,0、charaimg2は60,0の位置に描画
-    charaimg1.onload=function(){characanvas.drawImage(charaimg1,0,0,60,60)}
-    charaimg2.onload=function(){characanvas.drawImage(charaimg2,60,0,60,60)}
+    characanvas.width=120, characanvas.height=60;
+    var characanvasctx=characanvas.getContext("2d"); //charaimg1は0,0、charaimg2は60,0の位置に描画
+    charaimg1.onload=function(){characanvasctx.drawImage(charaimg1,0,0,60,60)}
+    charaimg2.onload=function(){characanvasctx.drawImage(charaimg2,60,0,60,60)}
 }
 function fieldMain() {
     const charasize=30; //キャラクターのサイズ
@@ -18,5 +20,6 @@ function fieldMain() {
     @param なし
     @return なし
     */
-    ctx2d.drawImage(characanvas,0,0,60,60,(width-charasize)/2,(height-charasize)/2);
+    walkanimation=(walkanimation+1)%30;
+    ctx2d.drawImage(characanvas,60*Math.floor(walkanimation/15),0,60,60,(width-charasize)/2,(height-charasize)/2,60,60);
 }

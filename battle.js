f0390e675888e17f101dbe39bf8c364b121fc94a
takuya_bob <takuya.egyzbob8416@gmail.com>
2021-12-05 19:52:19 +0900

    battlefunc1

--- battle.js
+++ battle.js
@@ -16,5 +16,36 @@ function battleMain() {
     ctx2d.fillStyle=red;
     ctx2d.fillRect(600,200,50,50);
     
-
 }
+
+var attackorder;//攻撃の順番
+
+function hitorder(){//先攻後攻決め: floor(素早さ*(乱数0.95-1.05))
+    var A_speed = Math.floor(mySpeed*(0.95*(1.05-0.95)*Math.random()));
+    var B_speed = Math.floor(enemySpeed*(0.95*(1.05-0.95)*Math.random()));
+    if(A_speed>=B_speed) attackorder=true;//味方の方が速い
+    else attackorder=false;
+}
+
+function hitcount(){//攻撃回数: Hitcount=((自分の素早さ)/(敵の素早さ)*16/10)
+    if(attackorder) return Math.floor(mySpeed/enemySpeed*16/10);
+    else return Math.floor(enemySpeed/mySpeed*16/10);
+}
+
+function hitodds(){//命中確率: (技の命中率*((100-敵の運)/100))
+    return (hitrate*((100-enemyLucky)/100))
+}
+
+function calcDamage(){//ダメージ計算
+    return Math.floor(Math.floor(Math.floor(level*2/4+2)* power * attack/enemydefend+2) * typeMatch * (0.9*(1.1-0.9)*Math.random()));
+}
+
+function needEx(level){//(レベル)^2.5
+    return Math.floor(Math.pow(level, 2.5))
+}
+
+function getEx(){//戦闘後獲得する経験値
+    return Math.floor(Math.floor(50*enemylevel*(0.9*(1.1-0.9)*Math.random()))*itemBonus*BossBonus)
+}
+
+function prob

9cfe3ce9bb78fc9eb860838142e93566194c9c00
yodai49 <cbasicjavaasembler@yahoo.co.jp>
2021-12-11 09:37:27 +0900

    procreate status fix

--- field.js
+++ field.js
@@ -97,8 +97,8 @@ function procreateProcess(){ //卵の孵化処理
     for(var i=0;i<12;i++) neweggData[i] = eggData[selectEggKind][i];
     neweggData[2]*= (1-randomrate+randomrate*Math.random())*Math.min(1.5,(0.8+0.07*lineCline));//HP
     neweggData[3]*= (1-randomrate+randomrate*Math.random())*Math.min(1.5,(0.8+0.07*lineCarc));//MP
-    neweggData[4]*= (1-randomrate+randomrate*Math.random())*Math.min(1.2,(0.9+0.4*Math.pow(lineratio,1)));//攻撃力
-    neweggData[5]*= (1-randomrate+randomrate*Math.random())*Math.min(1.2,(0.9+0.4*Math.pow((1-lineratio),1)));//防御力
+    neweggData[4]*= (1-randomrate+randomrate*Math.random())*Math.min(1.6,(0.9+0.8*Math.pow(lineratio,1)));//攻撃力
+    neweggData[5]*= (1-randomrate+randomrate*Math.random())*Math.min(1.6,(0.9+0.8*Math.pow((1-lineratio),1)));//防御力
     neweggData[7]+= 3*(randomrate*Math.random()+(1-randomrate)*Math.min(1,0.3*Math.sqrt(arcCarc)));//運
     neweggData[8]*= (1-randomrate+randomrate*Math.random())*Math.max(1.6,(1.5-(linecnt+arccnt)/15*0.8));//すばやさ
     neweggData[5]*= Math.min(1.5,(0.7+(linecnt+arccnt)/15*0.8));//防御力
